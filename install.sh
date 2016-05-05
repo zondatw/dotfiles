1b89432b1734e2f9c9c75496be4187f319380586
zondaTW <u226699@yahoo.com.tw>
2016-05-05 23:30:07 +0800

    Add install.sh(test) and install_script.sh(test)

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# install 
+apt-get install tmux
+apt-get install vim
+apt-get install ipython
+
---
+++ install_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#cp script
+cp .bashrc ~/.
+cp .tmux.conf ~/.
+cp .vimrc ~/.
+
+# vim color
+mkdir ~/.vim/colors
+cp molokai.vim ~/.vim/colors/
