119e0932fff34ea5b9a72e7c0325aced14b8a8eb
zondaTW <u226699@yahoo.com.tw>
2016-05-06 08:56:14 +0800

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -10,7 +10,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "Please type :PluginInstall in vim"
 
 # vim color
-test -d ~/.vim && mkdir ~/.vim/color || (mkdir ~/.vim && mkdir ~/.vim/color)
+test -d ~/.vim && mkdir ~/.vim/colors || (mkdir ~/.vim && mkdir ~/.vim/colors)
 cp molokai.vim ~/.vim/colors/
 
 
