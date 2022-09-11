16ebe3c5960130771ec4aac86601c0de89125144
zonda yang <u226699@gmail.com>
2022-09-11 18:54:58 +0800

    Add docker setting for nixos

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -102,11 +102,13 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  virtualisation.docker.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.zonda = {
     isNormalUser = true;
     description = "Zonda";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [
       firefox
       kate
@@ -122,6 +124,7 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+    docker
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
