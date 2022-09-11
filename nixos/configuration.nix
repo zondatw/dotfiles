b13c38d4224e6ee96a36ede53a8aee74e157daf7
zonda yang <u226699@gmail.com>
2022-09-11 18:54:19 +0800

    Add experimental setting for nixos

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -15,6 +15,12 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
+  nix = {
+    extraOptions = ''
+      experimental-features = nix-command
+    '';
+  };
+
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
