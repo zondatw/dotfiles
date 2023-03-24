5e1f4b291d6fb1933b29ee10edd0a2efeb9ae659
zonda yang <u226699@gmail.com>
2023-03-25 00:00:53 +0800

    Add arch_install_tools.sh

--- arch_install_tools.sh
+++ arch_install_tools.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo pacman -S --needed vim tmux git flameshot curl wget whois screenfetch libreoffice bind tree vivaldi xclip jq ranger zip ffmpeg vlc bash-completion ctags
+sudo pacman -S --needed vim tmux git flameshot curl wget whois screenfetch libreoffice bind tree vivaldi xclip jq ranger zip ffmpeg vlc bash-completion ctags \
+    trash-cli rust iotop iftop 
